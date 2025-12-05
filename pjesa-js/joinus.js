0d96875b5476b9c04821bec85bfecba7a2b6d147
ademajeuron8 <euron.ademaj@student.uni-pr.edu>
2025-12-05 22:49:32 +0100

    javascript-kontrollimi i adreses a eshte valide

--- pjesa-js/joinus.js
+++ pjesa-js/joinus.js
@@ -1,23 +1,32 @@
-/*i lejme nje emer FORMES */
 const forma = document.getElementById('formaRegjistrimit');
-  
-  const fushaEmail = document.getElementById('email');
+const fushaEmail = document.getElementById('email');
 
-  /* Kur te klikohet Sign Up */
-  forma.addEventListener('submit', function(event) {
-      
-      /*Faqja nuk behet refresh */
-      event.preventDefault();
+/* Kur të klikoni Sign Up */
+forma.addEventListener('submit', function(event) {
+    event.preventDefault(); // Ndaluam refresh-in e faqeve
 
-      // Shfaqim dritaren e suksesit
-      Swal.fire({
-          title: 'Sukses!',
-          text: 'Faleminderit! U regjistruat me sukses.',
-          icon: 'success',
-          confirmButtonText: 'Në rregull',
-          confirmButtonColor: '#e69500' 
-      });
-
-      /*Fshihet emaili pasi te shtypet butoni */
-      fushaEmail.value = '';
-  });
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    
+    if (emailPattern.test(fushaEmail.value)) {
+        // Shfaqim mesazhin e suksesit
+        Swal.fire({
+            title: 'Sukses!',
+            text: 'Faleminderit! U regjistruat me sukses.',
+            icon: 'success',
+            confirmButtonText: 'Në rregull',
+            confirmButtonColor: '#e69500'
+        });
+        
+        // Përsëri pastrojmë fushën e email-it pasi të shtypet butoni
+        fushaEmail.value = '';
+    } else {
+        // Nëse emaili është i pavlefshëm
+        Swal.fire({
+            title: 'Gabim!',
+            text: 'Ju lutemi futni një email të vlefshëm.',
+            icon: 'error',
+            confirmButtonText: 'Në rregull',
+            confirmButtonColor: '#e69500'
+        });
+    }
+});
